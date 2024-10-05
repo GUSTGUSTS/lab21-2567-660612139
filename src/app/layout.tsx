ef94338bcb19c1c08b5657bb55e1be9f5a7786a1
GUSTGUSTS <141816627+GUSTGUSTS@users.noreply.github.com>
2024-10-05 20:00:57 +0700

    t1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -80,8 +80,8 @@ export default function RootLayout({
               </Title>
               {children}
               <Footer
-                studentId="660610999"
-                fullName="Dome Potikanond"
+                studentId="660612139"
+                fullName="Kanathip Chiengthong"
                 year="2024"
               />
             </Container>
